b42db608fe413cf777ac335ca8151d5fea20ff8b
Emmy Belloni <emmybelloni@eduroam-172-150.dyn.carleton.edu>
2021-05-03 20:35:37 -0500

    bug testing datasource

--- web/backend/datasource.py
+++ web/backend/datasource.py
@@ -55,5 +55,5 @@ if __name__ == '__main__':
     connection = connect()
     data = DataSource()
     #Testing two variable table API
-    print(data.createTwoVariableTable(connect, "LonelinessFrequency", "LeftOut"))
+    print(data.createTwoVariableTable(data, "LonelinessFrequency", "LeftOut"))
     connection.close()
