6f9ef98ce5f9875ef694fa8c5fe3089276e5b42e
Emmy Belloni <emmybelloni@eduroam-172-150.dyn.carleton.edu>
2021-05-08 16:47:26 -0500

    tdd process

--- web/backend/testDatasource.py
+++ web/backend/testDatasource.py
@@ -8,6 +8,12 @@ class TestDataSource (unittest.TestCase):
         var2 = "Drugs"
         data = DataSource()
         self.assertEqual(data.chooseMethod(var1, var2), data.getTwoVariables(var1, var2))
+        
+    def test_one_variable(self):
+        var1 = "Internet"
+        var2 = "None"
+        data = DataSource()
+        self.assertEqual(data.chooseMethod(var1, var2), data.getOneVariable(var1))
 
 if __name__ == "__main__":
     unittest.main()
