cf8634148dae85bfbf9f237be7b0e171bb804ec5
Emmy Belloni <emmybelloni@eduroam-172-150.dyn.carleton.edu>
2021-05-08 16:44:47 -0500

    tdd process

--- web/backend/datasource.py
+++ web/backend/datasource.py
@@ -39,7 +39,7 @@ class DataSource:
         Returns:
             list: The output of either getTwoVariables() or getOneVariable()
         '''
-        pass
+        return getTwoVariables(x,y)
 
     def getTwoVariables(self, x, y):
         '''
