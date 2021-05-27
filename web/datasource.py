0a332471db439e40ab0a4e7d5de70b3a1ddb7cd0
Emmy Belloni <emmybelloni@eduroam-172-150.dyn.carleton.edu>
2021-05-27 15:30:11 -0500

    fixing up added questions

--- web/datasource.py
+++ web/datasource.py
@@ -144,11 +144,12 @@ class DataSource:
                     density.append(1)
             '''for i in range(len(density)):
                 density[i] = density[i] * 3'''
+            plt.rc('xtick', labelsize=5)
+            plt.rc('ytick', labelsize=5)
             plt.clf()
             plt.scatter(xaxis, yaxis, s=density)
-            plt.rc('xtick', labelsize=5)
-            plt.yticks(rotation=90)
-            plt.rc('ytick', labelsize=5)
+            plt.yticks(rotation=45)
+            plt.xticks(rotation=135)
             plt.xlabel(x)
             plt.ylabel(y)
         
