f6ea98adf7afe8534034644e6c0148e56e5592bf
Emmy Belloni <emmybelloni@eduroam-172-150.dyn.carleton.edu>
2021-05-27 15:23:37 -0500

    fixing up added questions

--- web/datasource.py
+++ web/datasource.py
@@ -147,7 +147,7 @@ class DataSource:
             plt.rc('xtick', labelsize=5)
             plt.xticks(rotation=45)
             plt.rc('ytick', labelsize=5)
-            plt.yticks(rotation=45)
+            plt.yticks(rotation=90)
             plt.clf()
             plt.scatter(xaxis, yaxis, s=density)
             plt.xlabel(x)
